22164593acbeeead5997a47d350c801a4f2eab44
KKyohei <kkym_a4@icloud.com>
2021-05-19 19:29:46 +0900

    Add WindowsFormsApp1

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbHeight = new System.Windows.Forms.TextBox();
+            this.tbWeight = new System.Windows.Forms.TextBox();
+            this.calculate = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.search = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbHeight
+            // 
+            this.tbHeight.Location = new System.Drawing.Point(110, 39);
+            this.tbHeight.Name = "tbHeight";
+            this.tbHeight.Size = new System.Drawing.Size(100, 19);
+            this.tbHeight.TabIndex = 0;
+            // 
+            // tbWeight
+            // 
+            this.tbWeight.Location = new System.Drawing.Point(328, 39);
+            this.tbWeight.Name = "tbWeight";
+            this.tbWeight.Size = new System.Drawing.Size(100, 19);
+            this.tbWeight.TabIndex = 1;
+            // 
+            // calculate
+            // 
+            this.calculate.Location = new System.Drawing.Point(211, 102);
+            this.calculate.Name = "calculate";
+            this.calculate.Size = new System.Drawing.Size(75, 23);
+            this.calculate.TabIndex = 2;
+            this.calculate.Text = "BMI";
+            this.calculate.UseVisualStyleBackColor = true;
+            this.calculate.Click += new System.EventHandler(this.calculate_Click);
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(230, 202);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 12);
+            this.result.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Height[cm]";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(263, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Weight[kg]";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(84, 106);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(100, 19);
+            this.tbName.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 12);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Your Name.";
+            // 
+            // search
+            // 
+            this.search.Location = new System.Drawing.Point(328, 102);
+            this.search.Name = "search";
+            this.search.Size = new System.Drawing.Size(75, 23);
+            this.search.TabIndex = 8;
+            this.search.Text = "Search";
+            this.search.UseVisualStyleBackColor = true;
+            this.search.Click += new System.EventHandler(this.search_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 268);
+            this.Controls.Add(this.search);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.calculate);
+            this.Controls.Add(this.tbWeight);
+            this.Controls.Add(this.tbHeight);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbHeight;
+        private System.Windows.Forms.TextBox tbWeight;
+        private System.Windows.Forms.Button calculate;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button search;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /* BMI を計算するボタンハンドラ */
+        private void calculate_Click(object sender, EventArgs e)
+        {
+            double h, w;
+            var data = new List<string>();
+
+            h = int.Parse(tbHeight.Text);
+            w = int.Parse(tbWeight.Text);
+
+            result.Text = (w / h / h * 10000.0).ToString();
+
+            data.Add(tbName.Text);
+            data.Add(h.ToString());
+            data.Add(w.ToString());
+            data.Add(result.Text);
+            csv_Output(data);
+        }
+
+        /* ファイル出力 */
+        private void csv_Output(List<string> data)
+        {
+            csv_Search(data[0], true);
+
+            using (StreamWriter sw = new StreamWriter("test.csv", true))
+            {
+                sw.WriteLine(String.Join(",", data));
+            }
+        }
+
+        /* ファイルから読み出しまたはレコードの削除を行う */
+        /* <delete> true: 削除 false: 読み出し */
+        private string[] csv_Search(string name, bool delete)
+        {
+            string[] ret = { };
+            var sl = new List<string>();
+
+            using (StreamReader sr = new StreamReader("test.csv"))
+            {
+                if (delete)
+                {
+                    while (sr.EndOfStream == false)
+                    {
+                        string s;
+                        string[] sa;
+
+                        s = sr.ReadLine();
+                        sa = s.Split(',');
+
+                        if (sa[0] != name)
+                        {
+                            // 指定されたもの以外を書き出すことで削除を実現
+                            sl.Add(s);
+                        }
+                    }
+                }
+                else
+                {
+                    while (sr.EndOfStream == false)
+                    {
+                        string[] s;
+
+                        s = sr.ReadLine().Split(',');
+
+                        if (s[0] == name)
+                        {
+                            ret = s;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (sl.Count != 0)
+            {
+                using (StreamWriter sw = new StreamWriter("test.csv"))
+                {
+                    foreach (var item in sl)
+                    {
+                        sw.WriteLine(item);
+                    }
+                }
+            }
+
+            return ret;
+        }
+
+        /* ファイル内を検索するボタンハンドラ */
+        private void search_Click(object sender, EventArgs e)
+        {
+            string[] s;
+
+            s = csv_Search(tbName.Text, false);
+
+            if (s.Length != 0)
+            {
+                tbName.Text = s[0];
+                tbHeight.Text = s[1];
+                tbWeight.Text = s[2];
+                result.Text = s[3];
+            }
+        }
+    }
+}
